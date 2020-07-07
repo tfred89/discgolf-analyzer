3981467a004058faadaabaea6c31a97d93d24ca7
Trent Frederick <trentfrederick@gmail.com>
2020-07-07 15:56:04 -0500

    continued models

--- discSite/bag/admin.py
+++ discSite/bag/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from bag import models
 
 # Register your models here.
+admin.register(models.BaseDisc)
+admin.register(models.Manufacturer)
+admin.register(models.Plastic)
+admin.register(models.Disc)
--- discSite/bag/models.py
+++ discSite/bag/models.py
@@ -1,20 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 # Create your models here.
-class Disc(models.Model):
-    name = models.CharField(max_length=120)
-    speed = models.FloatField(blank=True, null=True)
-    glide = models.FloatField(blank=True, null=True)
-    turn = models.FloatField(blank=True, null=True)
-    fade = models.FloatField(blank=True, null=True)
-    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE, related_name='mfg')
 
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ['speed']
-    
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=120)
@@ -25,8 +15,51 @@ class Manufacturer(models.Model):
         return self.name
 
 
+class BaseDisc(models.Model):
+    name = models.CharField(max_length=120)
+    speed = models.FloatField(blank=True, null=True)
+    glide = models.FloatField(blank=True, null=True)
+    turn = models.FloatField(blank=True, null=True)
+    fade = models.FloatField(blank=True, null=True)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='discs')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['speed']
+    
+
+class Disc(BaseDisc):
+    # represents an individual disc
+    weight = models.FloatField(blank=True, null=True)
+    primary_color = models.CharField(max_length=120, blank=True, null=True)
+    secondary_color = models.CharField(max_length=120, blank=True, null=True)
+    condition = models.IntegerField(blank=True, null=True)
+    plastic = models.ForeignKey('Plastic', on_delete=models.SET_NULL, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='discs')
+    public = models.BooleanField(default=False)
+    fortrade = models.BooleanField(default=False)
+    forsale = models.BooleanField(default=False)
+
+
+
 class Plastic(models.Model):
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='mfg')
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='plastic')
     name = models.CharField(max_length=80)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
+
+
+class Inventory(models.Model):
+    #  add through model
+    pass
+
+
+class Bag(models.Model):
+    name = models.CharField(max_length=80, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='bags')
+    description = models.TextField(blank=True, null=True)
+    discs = models.ManyToManyField(Disc)
+
 
